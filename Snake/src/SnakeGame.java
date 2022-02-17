951c3dcb01a9d7a15002e39ba7fb629c06ff22f1
Evan Cooke <evncooke@gmail.com>
2022-02-17 00:43:34 -0600

    Removed Score and Game over text at start of game.

--- Snake/src/SnakeGame.java
+++ Snake/src/SnakeGame.java
@@ -1,8 +1,13 @@
 // This game is based off of "Java snake game" by Bro Code on YouTube
+// https://www.youtube.com/watch?v=bI6e6qjJ8JQ
 
 // future features:
 // 1. add 2 snakes to control at once
-// 2. add game mode where speed of snake increases as apples get eaten
+// 2. add game mode where speed of snake increases as apples get eaten ------ DONE
+// 3. ability to restart after game ends
+// 4. make UI look somewhat pretty
+//
+// NO MORE, DON'T TAKE TOO LONG ON THIS, WE NEED BPM METRONOME WEBSITE
 
 public class SnakeGame {
 
